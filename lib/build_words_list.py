0c36911fbd5dcb75a56a04f2ba4475d51be7a6c0
AHpx <ahpx@yandex.com>
2025-10-20 15:22:18 +0800

    Add builder

--- lib/build_words_list.py
+++ lib/build_words_list.py
@@ -1,10 +1,19 @@
+from os import remove
 from pathlib import Path
 from wordfreq import top_n_list
 
-def build_words_list(path: str = 'words.txt') -> None:
+def build_words_list(path: str = 'words.txt', lang: str = 'en', size: int = 26000) -> None:
   file = Path(path)
   if not file.exists():
-    words = top_n_list('en', 26000)
-    file.write_text('\n'.join(words))
+    words = top_n_list(lang, size)
+    # Filter out unwanted words
+    filtered_words = [
+      word for word in words
+      if len(word) > 1 and word.isalpha()
+    ]
+    file.write_text('\n'.join(filtered_words))
+  else:
+    remove(file)
+    build_words_list(path)
 
 build_words_list()
